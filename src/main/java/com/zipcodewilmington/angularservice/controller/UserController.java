c42b306e9e659440eda1bf7202cd2514c9678202
wesley <wesley@zipcoders-MacBook-Pro-8.local>
2017-12-21 16:17:51 -0500

    saving

--- src/main/java/com/zipcodewilmington/angularservice/controller/UserController.java
+++ src/main/java/com/zipcodewilmington/angularservice/controller/UserController.java
@@ -2,22 +2,59 @@ package com.zipcodewilmington.angularservice.controller;
 
 import com.zipcodewilmington.angularservice.model.User;
 import com.zipcodewilmington.angularservice.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 /**
  * Created by leon on 12/20/17.
  */
-@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping(value = "/users")
+@CrossOrigin(origins = "http://localhost:4200")
+
 public class UserController {
+
+    Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
-    UserService userService;
+    private UserService userService;
 
     @RequestMapping(method = RequestMethod.POST)
-    public User saveUser(@RequestBody User user) {
-        userService.add(user);
-        return user;
+    public ResponseEntity<Void> saveUser(@RequestBody User user) {
+
+        User user1  = userService.add(user);
+
+        logger.info("adding user");
+
+        URI userURI = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("{id}")
+                .buildAndExpand(user1.getId())
+                .toUri();
+
+        return ResponseEntity.created(userURI).build();
+    }
+
+    @GetMapping()
+    public ResponseEntity<Iterable<User>> getAllUser(){
+        Iterable<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users,HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id){
+        User user = userService.getById(id);
+        if (user == null){
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+        }
+        return new ResponseEntity<>(user,HttpStatus.OK);
     }
 }
--- src/main/java/com/zipcodewilmington/angularservice/model/User.java
+++ src/main/java/com/zipcodewilmington/angularservice/model/User.java
@@ -1,10 +1,18 @@
 package com.zipcodewilmington.angularservice.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
+@Entity
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
 	private Long id;
+
     private String firstName;
 
 	public User() {}
